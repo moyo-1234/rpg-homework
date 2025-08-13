4fb0b0108fe56c53ffc40d40b85ae8667c98f4ce
moyo-1234 <folake.abudiore@gmail.com>
2025-08-13 18:39:32 +0100

    Add files via upload

--- Rw.py
+++ Rw.py
@@ -5,6 +5,7 @@ WIDTH =600
 HEIGHT = 383
 Gameover = 0
 objects = ["cricket","soccer"]
+win = False
 def arrange_objects():
     Act = []
     Apos = [100,500]
@@ -20,7 +21,9 @@ items = arrange_objects()
 def draw():
     screen.blit("bg",(0,0))
     if Gameover==1:
-        screen.draw.text("GAME OVER",fontsize = 20,color = "black",center = (370,160))
+        screen.draw.text("GAME OVER",fontsize = 55,color = "red",center = (300,160))
+    elif win == True:
+        screen.draw.text("YOU WIN",fontsize = 55,color = "green",center = (300,160))
     else:
         for i in items:
             i.draw()
@@ -31,13 +34,14 @@ def update():
             i.y = i.y + 1
 
 def on_mouse_down(pos):
-    global Gameover
+    global Gameover,win
     for i in items:
         if i.collidepoint(pos):
             
             if "cricket" in i.image:
                 screen.draw.text("YOU WIN",fontsize = 20,color = "black",center = (370,160))
                 print("You win")
+                win = True
                 break
             else:
                 Gameover = Gameover + 1
